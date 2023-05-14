4cc30db2bc42f6f169c7685a434e809c76c4e149
yuki ueno <yuki.weaver1204@gmail.com>
2023-05-14 12:55:17 +0900

    useRef

--- src/Counter.tsx
+++ src/Counter.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import { render } from "@testing-library/react";
+import React, { useEffect, useRef, useState } from "react";
 
 const Counter: React.FC<{}> = () => {
   const [value, setValue] = useState<number>(0);
@@ -13,11 +14,28 @@ const Counter: React.FC<{}> = () => {
     setValue((prevState) => prevState - 1);
   }
 
+  const renderTimes = useRef<number>(0);
+  useEffect(() => {
+    renderTimes.current = renderTimes.current + 1;
+  });
+
+  const ref = useRef<HTMLInputElement>(null!);
+  const focusInput = () => {
+    // const current = ref.current;
+    // if (current != null) {
+    //     current.focus();
+    // }
+    ref.current.focus();
+  }
+
     return (
       <div>
         <div>value: { value }</div>
         <button onClick={ increment }>+1</button>
         <button onClick={ decrement }>-1</button>
+        <div>This component was re-rendered { renderTimes.current } times!</div>
+        <input ref={ref} type="text" />
+        <button onClick={focusInput}>Click Me!</button>
       </div>
     );
 };
